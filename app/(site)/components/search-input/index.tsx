7dd337a186a4f5210c340b67cb015ce7f4e36f35
unknown <2020583117@qq.com>
2025-01-14 23:03:25 +0800

    feat: 可进行搜索

--- app/(site)/components/search-input/index.tsx
+++ app/(site)/components/search-input/index.tsx
@@ -1,6 +1,14 @@
 "use client"
 
+import { useEffect, useState } from "react"
+import fetchJsonp from "fetch-jsonp"
+import { motion, AnimatePresence } from "framer-motion"
+import { ArrowRightLeft } from "lucide-react"
+
 import { PlaceholdersAndVanishInput } from "@/components/ui/placeholders-and-vanish-input"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { cn } from "@/lib/utils"
+import { EngList } from "@/config/site"
 
 const SearchInput = () => {
   const placeholders = [
@@ -10,20 +18,106 @@ const SearchInput = () => {
     "Write a Javascript method to reverse a string",
     "How to assemble your own PC?"
   ]
+  const [activeEng, setActiveEng] = useState<(typeof EngList)[number]>(EngList[0])
+  const [keyword, setKeyword] = useState<string>("")
+  const [suggestion, setSuggestion] = useState<{ type: string; sa: string; q: string }[]>([])
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value)
+    setKeyword(e.target.value)
   }
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    window.open(`${activeEng.href}${keyword}`)
+    setKeyword("")
     e.preventDefault()
     console.log("submitted")
   }
+  const handleSuggestion = (q: string) => {
+    window.open(`${activeEng.href}${q}`)
+  }
+  useEffect(() => {
+    let timer: NodeJS.Timeout | null = null
+
+    if (keyword) {
+      timer = setTimeout(() => {
+        const encodedKeyword = encodeURIComponent(keyword)
+        const url = `https://www.baidu.com/sugrec?ie=utf-8&json=1&prod=pc&wd=${encodedKeyword}&_time=${Date.now()}&callback=jsonpCallback`
+
+        // 定义全局回调函数
+        window.jsonpCallback = (data) => {
+          console.log(data) // 打印返回的数据
+          if (data && data.g) {
+            setSuggestion(data.g) // 更新建议列表
+          }
+        }
+
+        // 发起 JSONP 请求
+        fetchJsonp(url)
+          .then((response) => response.json()) // 解析 JSON 数据
+          .then((data) => {
+            console.log("Parsed JSON:", data)
+            setSuggestion(data.g)
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error)
+          })
+
+        // 清理函数以避免内存泄漏
+        return () => {
+          delete window.jsonpCallback // 清除全局回调函数
+        }
+      }, 300)
+    } else {
+      setSuggestion([])
+    }
+
+    return () => {
+      if (timer) {
+        clearTimeout(timer)
+      }
+    }
+  }, [keyword])
+
   return (
-    <div className="flex h-[40rem] flex-col items-center justify-center px-4">
-      <h2 className="mb-10 text-center text-xl text-black dark:text-white sm:mb-20 sm:text-5xl">
-        Ask Aceternity UI Anything
-      </h2>
+    <div className="relative flex h-[40rem] flex-col items-center justify-center px-4">
+      <h2 className="mb-10 text-center text-xl sm:mb-20 sm:text-5xl">Ask Aceternity UI Anything</h2>
       <PlaceholdersAndVanishInput placeholders={placeholders} onChange={handleChange} onSubmit={onSubmit} />
+      <AnimatePresence>
+        {keyword && (
+          <motion.div
+            key="scroll-area"
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.95 }}
+            transition={{ duration: 0.3 }}
+            className="absolute top-[65%] z-10 h-auto w-full max-w-xl rounded-md border-0 bg-background py-1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),_0px_1px_0px_0px_rgba(25,28,33,0.02),_0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:bg-zinc-800"
+          >
+            <ScrollArea>
+              <p
+                className={cn(
+                  "flex cursor-pointer rounded-md py-1 pl-4 transition-all duration-300 sm:pl-10",
+                  "hover:bg-black/20 hover:bg-opacity-50 hover:pl-8"
+                )}
+              >
+                <ArrowRightLeft />
+                翻译一下
+              </p>
+              {suggestion?.map(({ q, sa }) => (
+                <p
+                  key={sa}
+                  className={cn(
+                    "cursor-pointer rounded-md py-1 pl-4 transition-all duration-300 sm:pl-10",
+                    "hover:bg-accent hover:pl-8 hover:text-accent-foreground"
+                  )}
+                  onClick={() => handleSuggestion(q)}
+                >
+                  {q}
+                </p>
+              ))}
+            </ScrollArea>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
--- config/site.ts
+++ config/site.ts
@@ -1,85 +1,86 @@
-import { FaBlog, FaBook, FaPaperPlane } from 'react-icons/fa'
-import { AiFillGithub, AiFillWechat, AiFillMail, AiFillTwitterCircle } from 'react-icons/ai'
-import { SiBaidu, SiMicrosoftbing, SiGoogle, SiGithub, SiZhihu, SiBilibili, SiSinaweibo } from 'react-icons/si'
+import { AiFillGithub, AiFillWechat, AiFillMail, AiFillTwitterCircle } from "react-icons/ai"
+import { SiBaidu, SiMicrosoftbing, SiGoogle, SiGithub, SiZhihu, SiBilibili, SiSinaweibo } from "react-icons/si"
+import { FaGithub, FaXTwitter } from "react-icons/fa6"
+import { Rss, Navigation, Search, House } from "lucide-react"
 
-const SiteInfoLinks = [
-  {
-    href: 'https://blog.liwenkai.fun',
-    icon: FaBlog,
-    text: 'Blog'
-  },
-  // {
-  //   href: 'https://www.liwenkai.asia/',
-  //   icon: FaMusic,
-  //   text: 'Music'
-  // },
-  {
-    href: 'https://nav.liwenkai.fun',
-    icon: FaBook,
-    text: 'Nav'
-  },
-  {
-    href: 'https://me.liwenkai.fun',
-    icon: FaPaperPlane,
-    text: 'Me'
-  }
+export const SiteConfig = {
+  title: "文凯 | liwenka1",
+  description: "文凯的个人主页"
+}
+
+export const PersonalInfo = {
+  name: "文凯",
+  title: "软件工程师 | 开源爱好者",
+  description: "Better Late than Never"
+}
+
+export const WebsiteLinks = [
+  { title: "Me", href: "https://me.liwenkai.fun", icon: House },
+  { title: "Blog", href: "https://blog.liwenkai.fun", icon: Rss },
+  { title: "Nav", href: "https://nav.liwenkai.fun", icon: Navigation },
+  { title: "Search", href: "https://s.liwenkai.fun", icon: Search }
+]
+
+export const SocialConfig = [
+  { title: "GitHub", href: "https://github.com/liwenka1", icon: FaGithub },
+  { title: "X", href: "https://x.com/liwenka1", icon: FaXTwitter }
 ]
 
 const ContactLinks = [
   {
     icon: AiFillGithub,
-    href: 'https://github.com/liwenka1'
+    href: "https://github.com/liwenka1"
   },
   {
     icon: AiFillWechat,
-    href: 'https://weixin.sogou.com/weixin?type=1&query=kk%E6%83%B3%E5%BD%93%E7%A8%8B%E5%BA%8F%E5%91%98'
+    href: "https://weixin.sogou.com/weixin?type=1&query=kk%E6%83%B3%E5%BD%93%E7%A8%8B%E5%BA%8F%E5%91%98"
   },
   {
     icon: AiFillMail,
-    href: 'mailto:2020583117@qq.com'
+    href: "mailto:2020583117@qq.com"
   },
   {
     icon: AiFillTwitterCircle,
-    href: 'https://twitter.com/liwenka1'
+    href: "https://twitter.com/liwenka1"
   }
 ]
 
 const EngList = [
   {
-    title: '百度',
+    title: "百度",
     icon: SiBaidu,
-    href: 'https://www.baidu.com/s?wd='
+    href: "https://www.baidu.com/s?wd="
   },
   {
-    title: '必应',
+    title: "必应",
     icon: SiMicrosoftbing,
-    href: 'https://www.bing.com/search?q='
+    href: "https://www.bing.com/search?q="
   },
   {
-    title: '谷歌',
+    title: "谷歌",
     icon: SiGoogle,
-    href: 'https://www.google.com/search?q='
+    href: "https://www.google.com/search?q="
   },
   {
-    title: 'Github',
+    title: "Github",
     icon: SiGithub,
-    href: 'https://github.com/search?q='
+    href: "https://github.com/search?q="
   },
   {
-    title: '知乎',
+    title: "知乎",
     icon: SiZhihu,
-    href: 'https://www.zhihu.com/search?q='
+    href: "https://www.zhihu.com/search?q="
   },
   {
-    title: 'Bilibili',
+    title: "Bilibili",
     icon: SiBilibili,
-    href: 'https://search.bilibili.com/all?keyword='
+    href: "https://search.bilibili.com/all?keyword="
   },
   {
-    title: '微博',
+    title: "微博",
     icon: SiSinaweibo,
-    href: 'https://s.weibo.com/weibo?q='
+    href: "https://s.weibo.com/weibo?q="
   }
 ]
 
-export { SiteInfoLinks, ContactLinks, EngList }
+export { ContactLinks, EngList }
